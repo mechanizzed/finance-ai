962f0947ec280d6de1d03902ecbfc0c15c33e2b8
Luciano J Guerra <dsibr@hotmail.com>
2025-05-17 22:19:35 -0300

    style: loader spinner color

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from "lucide-react";
 export default function Loading() {
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <Loader2 className="animate-spin" size={24} />
+      <Loader2 className="text-primary animate-spin" size={28} />
     </div>
   );
 }
